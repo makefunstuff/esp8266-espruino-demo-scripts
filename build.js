26b87035f54618976f431671c9dd54e044f15822
Iurii Plugatariov <plugatariov@gmail.com>
2016-03-06 14:46:25 +0200

    programmatically setup baudrate

--- build.js
+++ build.js
@@ -1,12 +1,22 @@
 var esp = require('espruino');
+var Promise = require('bluebird');
 
 var port = '/dev/cu.SLAB_USBtoUART';
 var filename = './helloworld.js';
 
-esp.init();
-esp.sendFile(port, filename, function() {
-  esp.expr(port, "save()", function(result) {
-    console.log('file hase been uploaded');
-    console.log(result);
+new Promise(function(resolve, reject) {
+  esp.init(function() {
+    resolve(Espruino)
+  })
+})
+.then(function(Espruino) {
+  Espruino.Config.BAUD_RATE = "115200"
+})
+.then(function() {
+  esp.sendFile(port, filename, function() {
+    esp.expr(port, "save()", function(result) {
+      console.log('file hase been uploaded');
+      console.log(result);
+    })
   })
 })
